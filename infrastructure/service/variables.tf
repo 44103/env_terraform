d07664828a878710f9c8298ac3bb60c69a6a37e8
44103 <universeengineer@gmail.com>
2020-08-13 17:29:05 +0900

    :tada: Module型の環境切り替え

---
+++ infrastructure/service/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "リージョン"
+  default     = "ap-northeast-1"
+}
+
+variable "prefix" {
+  description = "リソース名のPrefix"
+  default = "test"
+}
+
+variable "project" {
+  description = "リソース名のProject"
+  default = "sample"
+}
+
+variable "exe_env" {
+}
